91e960acb2867b16fcce33f8eff74bd43bbf3a11
Donte Graham <explorerstudio525@gmail.com>
2024-05-31 09:24:59 -0400

    added simple home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,43 +7,39 @@ export default async function Home() {
   const hello = await api.post.hello({ text: "from tRPC" });
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <p>Debug mastery</p>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
-        <div className="flex flex-col items-center gap-2">
-          <p className="text-2xl text-white">
-            {hello ? hello.greeting : "Loading tRPC query..."}
-          </p>
+    <main className="flex min-h-full flex-col items-center justify-center bg-gradient-to-b from-neutral-800 to-neutral-950">
+      <div className="container flex flex-col items-center justify-center gap-24 px-4 py-16 ">
+        <div className="flex flex-col items-center justify-center gap-4">
+          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+            Debug Mastery
+          </h1>
+
+          <h3 className="text-center text-2xl">
+            A website to highlight educational content creators for web
+            development.
+          </h3>
         </div>
 
-        <CrudShowcase />
+        <div className="flex gap-10">
+          <img
+            className=" h-48 w-48 rounded-full border border-gray-500"
+            src="https://utfs.io/f/f7ec4fab-c964-4936-bef4-472f482e29db-gonife.jpg"
+            alt="Theo Browne"
+          />
+          <img
+            className=" h-48 w-48  rounded-full border border-gray-500"
+            src="https://utfs.io/f/99c98833-e3e4-4e7f-92dc-889535e1f162-envsqj.jpg"
+            alt="Fireship.io"
+          />
+          <img
+            className=" h-48 w-48 rounded-full border border-gray-500"
+            src="https://utfs.io/f/11960624-972a-44c5-a0e0-2d11468c07d9-35m4pb.jpg"
+            alt="Primeagen"
+          />
+        </div>
+        <button className=" h-10 rounded-md bg-primary px-4 py-2 hover:bg-primary/85">
+          See Creators
+        </button>
       </div>
     </main>
   );

828046c3bdfc00416c868a2ca6ab1548b45c5453
Donte Graham <explorerstudio525@gmail.com>
2024-06-08 15:09:30 -0400

    Scaffold basic UI for creator page.

--- src/app/creators/[id]/page.tsx
+++ src/app/creators/[id]/page.tsx
@@ -58,7 +58,45 @@ const thirdItem = mockdata[2]!;
 const fourthItem = mockdata[3]!;
 
 function page({ params }: { params: { id: string } }) {
-  return <p>Creator id: {params.id}</p>;
+  return (
+    <div className="relative flex h-full flex-col items-center overflow-x-hidden">
+      {/* bg image */}
+      <div className="absolute top-0 z-[-10] h-[56.25vw] w-full">
+        <img
+          className="absolute top-0 h-[56.25vw] w-full bg-cover object-cover opacity-35"
+          src={sampleCreator.bannerImage}
+        />
+        <div className="absolute bottom-0 h-[14.7vw] w-full bg-gradient-to-b from-transparent via-gray-800 to-black"></div>
+        <p className="absolute left-1/2 top-1/3 -translate-x-1/2 -translate-y-1/2 text-center text-5xl text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
+          {sampleCreator.name}
+        </p>
+      </div>
+
+      <div className="pb-[30%]"></div>
+      <VideosCarousel />
+      <img
+        className="my-20 h-48 w-48 rounded-full border border-gray-500"
+        src={sampleCreator.profileImage}
+        alt={sampleCreator.name}
+      />
+
+      <h4 className="mb-3 text-3xl">About {sampleCreator.name}</h4>
+      <p className="max-w-[60ch] whitespace-pre-line text-center">
+        {sampleCreator.biography}
+      </p>
+      <div className="my-10 flex h-56 items-center gap-2">
+        <p> Visit Channel</p>
+        <a
+          href={`//www.youtube.com/@${sampleCreator.youtubeHandle}`}
+          target="_blank"
+        >
+          <svg width="64" height="64" fill="red" viewBox="0 0 16 16">
+            <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.01 2.01 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.01 2.01 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31 31 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.01 2.01 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A100 100 0 0 1 7.858 2zM6.4 5.209v4.818l4.157-2.408z" />
+          </svg>
+        </a>
+      </div>
+    </div>
+  );
 }
 
 function VideoItem({ props }: { props: VideoType }) {

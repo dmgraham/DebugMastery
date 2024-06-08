1f1e4f67cf5f7af3da4a0d0d400e826ad35e13c4
Donte Graham <explorerstudio525@gmail.com>
2024-06-08 14:12:47 -0400

    Added component for displaying a single creator video.

--- src/app/creators/[id]/page.tsx
+++ src/app/creators/[id]/page.tsx
@@ -54,4 +54,23 @@ function page({ params }: { params: { id: string } }) {
   return <p>Creator id: {params.id}</p>;
 }
 
+function VideoItem({ props }: { props: VideoType }) {
+  return (
+    <div className="flex flex-col gap-2 ">
+      <img className="aspect-video w-72" src={props.thumbnail} />
+
+      <div className="flex min-w-60 max-w-72 justify-between">
+        <h3 className="line-clamp-3 w-full text-ellipsis pl-1">
+          {props.title}
+        </h3>
+        <div className="flex w-7 cursor-pointer text-slate-300 hover:text-white">
+          <svg className="h-10 w-10" fill="currentColor" viewBox="0 4 16 16">
+            <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0" />
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default page;

38bf99a7450956f0521241d7c3ea1ad6cbc6989c
Donte Graham <explorerstudio525@gmail.com>
2024-06-19 16:18:47 -0400

    Added API to get content creator by id.

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,12 +1,14 @@
-import { postRouter } from "~/server/api/routers/post";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
+import { creatorRouter } from "./routers/creator";
 
 /**
  * This is the primary router for your server.
  *
  * All routers added in /api/routers should be manually added here.
  */
-export const appRouter = createTRPCRouter({});
+export const appRouter = createTRPCRouter({
+  creator: creatorRouter,
+});
 
 // export type definition of API
 export type AppRouter = typeof appRouter;

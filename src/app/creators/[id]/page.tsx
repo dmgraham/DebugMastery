9da33461ed5b7fe762153ce8b86800ee949e77bc
Donte Graham <explorerstudio525@gmail.com>
2024-08-06 19:37:55 -0400

    Removed mock data from creator's page

--- src/app/creators/[id]/page.tsx
+++ src/app/creators/[id]/page.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-import type { VideoType } from "~/app/_components/VideoItem";
 import VideosCarousel from "~/app/_components/VideosCarousel";
 
 interface Creator {
@@ -25,35 +23,6 @@ Beyond his tutorials, Theo fosters a vibrant community where developers can exch
     "https://utfs.io/f/5685b4ae-a679-408f-837a-32d1573c255f-n4g0bh.png",
   youtubeHandle: "t3dotgg",
 };
-
-const mockdata: VideoType[] = [
-  {
-    title: "Dan Abramov SLAYS Frontend Interview w/ Ex-Twitch Engineer",
-    thumbnail:
-      "https://utfs.io/f/d11daa98-469e-4382-a43a-9e818f806bb1-kpyxkd.jpg",
-  },
-  {
-    title:
-      "Comparing Modern CSS Solutions (Tailwind vs MUI vs Bootstrap vs Chakra vs...)",
-    thumbnail:
-      "https://utfs.io/f/d11daa98-469e-4382-a43a-9e818f806bb1-kpyxkd.jpg",
-  },
-  {
-    title: "JavaScript Framework Tier List",
-    thumbnail:
-      "https://utfs.io/f/d11daa98-469e-4382-a43a-9e818f806bb1-kpyxkd.jpg",
-  },
-  {
-    title: "The Biggest Change In React's History",
-    thumbnail:
-      "https://utfs.io/f/d11daa98-469e-4382-a43a-9e818f806bb1-kpyxkd.jpg",
-  },
-];
-
-const firstItem = mockdata[0]!;
-const secondItem = mockdata[1]!;
-const thirdItem = mockdata[2]!;
-const fourthItem = mockdata[3]!;
 
 function page({ params }: { params: { id: string } }) {
   return (

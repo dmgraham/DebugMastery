893f51344925e920e443c1247d4c19db3bb1b300
Donte Graham <explorerstudio525@gmail.com>
2024-08-09 22:38:29 -0400

    Added API to get creator's videos by id

--- src/server/api/routers/creator.ts
+++ src/server/api/routers/creator.ts
@@ -1,35 +1,54 @@
 import { z } from "zod";
 import { TRPCError } from "@trpc/server";
-
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 const PopularVideoLimit = 10;
 
 export const creatorRouter = createTRPCRouter({
-  getCreator: publicProcedure
-    .input(z.number())
-    .query(async ({ ctx, input }) => {
-      const creator = await ctx.db.debugMastery_Creator.findFirst({
-        where: {
-          id: input,
-        },
-        include: {
-          DebugMastery_Videos: {
-            select: {
-              title: true,
-              thumbnail: true,
-            },
-            take: PopularVideoLimit,
+  getCreator: publicProcedure.input(z.number()).query(async ({ ctx, input }) => {
+    const creator = await ctx.db.debugMastery_Creator.findFirst({
+      where: {
+        id: input,
+      },
+      include: {
+        DebugMastery_Videos: {
+          select: {
+            title: true,
+            thumbnail: true,
           },
+          take: PopularVideoLimit,
         },
+      },
+    });
+
+    if (!creator)
+      throw new TRPCError({
+        code: "NOT_FOUND",
+        message: "No creator with specified ID found.",
       });
 
-      if (!creator)
-        throw new TRPCError({
-          code: "NOT_FOUND",
-          message: "No creator with specified ID found.",
-        });
+    return creator;
+  }),
 
-      return creator;
-    }),
+  getCreatorVideos: publicProcedure.input(z.number()).query(async ({ ctx, input }) => {
+    const creatorVideos = await ctx.db.debugMastery_Videos.findMany({
+      where: {
+        debugMastery_CreatorId: input,
+      },
+      select: {
+        id: true,
+        title: true,
+        thumbnail: true,
+      },
+      take: PopularVideoLimit,
+    });
+
+    if (!creatorVideos)
+      throw new TRPCError({
+        code: "NOT_FOUND",
+        message: "No creator videos with specified ID found.",
+      });
+
+    return creatorVideos;
+  }),
 });

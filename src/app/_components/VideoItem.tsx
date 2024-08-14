643ade41ab36921a49eddc89c6599ac794623dec
Donte Graham <explorerstudio525@gmail.com>
2024-08-14 18:57:21 -0400

    Changed VideoItems to receive type definition from creator videos api

--- src/app/_components/VideoItem.tsx
+++ src/app/_components/VideoItem.tsx
@@ -1,18 +1,12 @@
-interface VideoType {
-  title: string;
-  thumbnail: string;
-  videoPreview?: string;
-}
+import { creatorVideoOutput as VideoType } from "~/server/api/routers/creator";
 
-function VideoItem({ props }: { props: VideoType }) {
+function VideoItem({ props }: { props: VideoType[number] }) {
   return (
     <div className="flex flex-[0_0_50.00%] flex-col gap-2 md:flex-[0_0_33.33%] lg:flex-[0_0_25.00%] xl:flex-[0_0_20.00%] 2xl:flex-[0_0_16.67%]">
       <img className="aspect-video w-72" src={props.thumbnail} />
 
       <div className="flex min-w-60 max-w-72 justify-between">
-        <h3 className="line-clamp-3 w-full text-ellipsis pl-1">
-          {props.title}
-        </h3>
+        <h3 className="line-clamp-3 w-full text-ellipsis pl-1">{props.title}</h3>
         <div className="flex w-7 cursor-pointer text-slate-300 hover:text-white">
           <svg className="h-10 w-10" fill="currentColor" viewBox="0 4 16 16">
             <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0" />

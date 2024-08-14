cd3b077baa514657c29bae37e00a5af19f27d125
Donte Graham <explorerstudio525@gmail.com>
2024-08-14 19:03:04 -0400

    updated mock data with mock Ids.

--- src/app/_components/videoCarouselMockData.ts
+++ src/app/_components/videoCarouselMockData.ts
@@ -1,31 +1,30 @@
-import { VideoType } from "./VideoItem";
+import { creatorVideoOutput } from "~/server/api/routers/creator";
 
-const mockdata: VideoType[] = [
+const mockdata: creatorVideoOutput = [
   {
+    id: 1,
     title: "Dan Abramov SLAYS Frontend Interview w/ Ex-Twitch Engineer",
-    thumbnail:
-      "https://utfs.io/f/d11daa98-469e-4382-a43a-9e818f806bb1-kpyxkd.jpg",
+    thumbnail: "https://utfs.io/f/d11daa98-469e-4382-a43a-9e818f806bb1-kpyxkd.jpg",
   },
   {
-    title:
-      "Comparing Modern CSS Solutions (Tailwind vs MUI vs Bootstrap vs Chakra vs...)",
-    thumbnail:
-      "https://utfs.io/f/d11daa98-469e-4382-a43a-9e818f806bb1-kpyxkd.jpg",
+    id: 2,
+    title: "Comparing Modern CSS Solutions (Tailwind vs MUI vs Bootstrap vs Chakra vs...)",
+    thumbnail: "https://utfs.io/f/d11daa98-469e-4382-a43a-9e818f806bb1-kpyxkd.jpg",
   },
   {
+    id: 3,
     title: "JavaScript Framework Tier List",
-    thumbnail:
-      "https://utfs.io/f/d11daa98-469e-4382-a43a-9e818f806bb1-kpyxkd.jpg",
+    thumbnail: "https://utfs.io/f/d11daa98-469e-4382-a43a-9e818f806bb1-kpyxkd.jpg",
   },
   {
+    id: 4,
     title: "The Biggest Change In React's History",
-    thumbnail:
-      "https://utfs.io/f/d11daa98-469e-4382-a43a-9e818f806bb1-kpyxkd.jpg",
+    thumbnail: "https://utfs.io/f/d11daa98-469e-4382-a43a-9e818f806bb1-kpyxkd.jpg",
   },
   {
+    id: 5,
     title: "I Coded In VR. It Went Better Than Expected.",
-    thumbnail:
-      "https://utfs.io/f/6479cac8-1adb-4352-a4d7-355ffeddd560-guta6s.jpg",
+    thumbnail: "https://utfs.io/f/6479cac8-1adb-4352-a4d7-355ffeddd560-guta6s.jpg",
   },
 ];
 

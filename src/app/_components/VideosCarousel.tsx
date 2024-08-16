fb4b5191a8e02546f241e82bf44af1121b9bf9db
Donte Graham <explorerstudio525@gmail.com>
2024-08-16 16:25:56 -0400

    Removed parent div and title header from video carousel component

--- src/app/_components/VideosCarousel.tsx
+++ src/app/_components/VideosCarousel.tsx
@@ -23,9 +23,11 @@ const maxVideosAtBreakpoints: Record<Breakpoint, number> = {
 
 function VideosCarousel({ videos }: { videos: creatorVideoOutput | Promise<creatorVideoOutput> }) {
   return (
-    <Suspense fallback={<CarouselContentPlaceHolder />}>
-      <CarouselContent videos={videos} />
-    </Suspense>
+    <div className="w-full self-start">
+      <Suspense fallback={<CarouselContentPlaceHolder />}>
+        <CarouselContent videos={videos} />
+      </Suspense>
+    </div>
   );
 }
 
@@ -82,44 +84,40 @@ function CarouselContent({ videos }: { videos: creatorVideoOutput | Promise<crea
   }
 
   return (
-    <div className="w-full self-start">
-      <h2 className="p-3 text-3xl font-bold">Popular Videos</h2>
+    <div className="relative">
+      <button
+        className="duration-50 absolute left-0 z-10 flex h-full w-[3%] items-center justify-center text-[transparent] transition-colors hover:bg-black/80 hover:text-white"
+        onClick={MoveCarouselLeft}
+      >
+        <svg width="32" height="32" fill="currentColor" viewBox="0 0 16 16">
+          <path
+            fill-rule="evenodd"
+            d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"
+          />
+        </svg>
+      </button>
 
-      <div className="relative">
-        <button
-          className="duration-50 absolute left-0 z-10 flex h-full w-[3%] items-center justify-center text-[transparent] transition-colors hover:bg-black/80 hover:text-white"
-          onClick={MoveCarouselLeft}
-        >
-          <svg width="32" height="32" fill="currentColor" viewBox="0 0 16 16">
-            <path
-              fill-rule="evenodd"
-              d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"
-            />
-          </svg>
-        </button>
+      <button
+        className="duration-50 absolute right-0 z-10 flex h-full w-[3%] items-center justify-center text-[transparent] transition-colors hover:bg-black/80 hover:text-white"
+        onClick={MoveCarouselRight}
+      >
+        <svg width="32" height="32" fill="currentColor" viewBox="0 0 16 16">
+          <path
+            fill-rule="evenodd"
+            d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"
+          />
+        </svg>
+      </button>
 
-        <button
-          className="duration-50 absolute right-0 z-10 flex h-full w-[3%] items-center justify-center text-[transparent] transition-colors hover:bg-black/80 hover:text-white"
-          onClick={MoveCarouselRight}
-        >
-          <svg width="32" height="32" fill="currentColor" viewBox="0 0 16 16">
-            <path
-              fill-rule="evenodd"
-              d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"
-            />
-          </svg>
-        </button>
-
-        <div
-          className="flex flex-1 gap-0 transition-transform duration-300"
-          style={{
-            transform: `translateX(-${xTranslation}px)`,
-          }}
-        >
-          {resolvedVideos.map((video) => {
-            return <VideoItem key={video.id} props={video} />;
-          })}
-        </div>
+      <div
+        className="flex flex-1 gap-0 transition-transform duration-300"
+        style={{
+          transform: `translateX(-${xTranslation}px)`,
+        }}
+      >
+        {resolvedVideos.map((video) => {
+          return <VideoItem key={video.id} props={video} />;
+        })}
       </div>
     </div>
   );
